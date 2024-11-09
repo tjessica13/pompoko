ed10b04ef38c7d6d68007d11c1db1e0ec629e238
tjessica13 <jtanottawau@gmail.com>
2024-11-08 21:22:34 -0500

    generate random number function

--- components/Pokemon.tsx
+++ components/Pokemon.tsx
@@ -8,11 +8,20 @@ interface PokemonProps {
 
 const Pokemon = ({ pokemonList }: PokemonProps) => {
 
+    // https://stackoverflow.com/questions/4959975/generate-random-number-between-two-numbers-in-javascript
+    // https://www.w3schools.com/js/js_random.asp
+    // function for generating a random number between 1 and 151
+    function getRandomNumber() {
+        return Math.floor(Math.random() * 151) + 1;
+    }
+
     return (
         <div>
             Pokemon Display Here
+            <div>{getRandomNumber()}</div>
             <div>
                 {
+                    
                     // print out 151 pokemon names
                     pokemonList.map((pokemon : any) => {
                         return (
--- lib/pokemonAPI.ts
+++ lib/pokemonAPI.ts
@@ -2,6 +2,7 @@
 // pokeapi link
 const POKEMON_API_LINK = "https://pokeapi.co/api/v2/";
 
+
 // get the first 151 pokemon using the api and return as a list
 export async function getPokemonList() {
     const response = await fetch(POKEMON_API_LINK + "pokemon?limit=151&offset=0");
