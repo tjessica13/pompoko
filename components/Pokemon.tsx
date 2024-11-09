60d7c505037a3ba2bc662a48ad67fbb2dff2fb24
tjessica13 <jtanottawau@gmail.com>
2024-11-08 21:44:32 -0500

    getting the gif link using the API

--- components/Pokemon.tsx
+++ components/Pokemon.tsx
@@ -1,35 +1,17 @@
-
+import Image from "next/image";
 
 // props for the pokemon component to use
 interface PokemonProps {
     // list from the api
-    pokemonList: any
+    pokemon : any
 }
 
-const Pokemon = ({ pokemonList }: PokemonProps) => {
-
-    // https://stackoverflow.com/questions/4959975/generate-random-number-between-two-numbers-in-javascript
-    // https://www.w3schools.com/js/js_random.asp
-    // function for generating a random number between 1 and 151
-    function getRandomNumber() {
-        return Math.floor(Math.random() * 151) + 1;
-    }
+const Pokemon = ({pokemon} : PokemonProps) => {
 
     return (
         <div>
             Pokemon Display Here
-            <div>{getRandomNumber()}</div>
-            <div>
-                {
-                    
-                    // print out 151 pokemon names
-                    pokemonList.map((pokemon : any) => {
-                        return (
-                            <p key={pokemon.name}>{pokemon.name}</p>
-                        )
-                    })
-                }
-            </div>
+            <div>{pokemon}</div>
         </div>
     );
 }
