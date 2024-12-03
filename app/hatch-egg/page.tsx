69f68260d08008b5d4bf96ae7a80a1ef2b66073a
tjessica13 <jtanottawau@gmail.com>
2024-12-03 18:45:46 -0500

    adding egg and font to egg hatching and adjusting css

--- app/hatch-egg/page.tsx
+++ app/hatch-egg/page.tsx
@@ -1,12 +1,30 @@
 import Link from "next/link";
+import Image from 'next/image';
+import ImageLoaderComponent from "@/components/ImageLoaderComponent";
+import { Jersey_10 } from 'next/font/google';
 
-// TODO: add egg image into this page
+const jersey = Jersey_10({ 
+  weight: ['400'],
+  subsets: ['latin']
+});
 
 export default async function Home() {
+
+  //path for which image to load
+  let imgSource = '/images/egg_png.png';
+
   return (
     <main className="hatch-egg">
-        <h1>Oh?</h1>
-        <Link href="/"><p>Continue</p></Link>
+        <div className="pokemon-element">
+                <div className="egg-image">
+                    <ImageLoaderComponent link= {imgSource}/>
+                </div>
+                <div className="egg-base">
+                    <Image alt="pokemon base" src="/images/Group 6.png" width={450} height={100}></Image>
+                </div>
+            </div>
+        <h1 className={jersey.className}>Oh?</h1>
+        <Link href="/"><p className={jersey.className}>Continue</p></Link>
     </main>
   );
 }
