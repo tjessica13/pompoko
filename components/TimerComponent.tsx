a01ccf80c4eadd7ff8f2e39f0d6be4465c8bad96
joannawang <jiaoni.wang.01@gmail.com>
2024-12-04 00:27:40 -0500

    added progress bars and empty/full icons

--- components/TimerComponent.tsx
+++ components/TimerComponent.tsx
@@ -16,6 +16,8 @@ let hatchCounter = 0;
 
 //path for which image to load
 let imgSource = '/images/egg_png.png';
+let progressBarIconFull = '/images/progress_bar.png';
+let progressBarIconEmpty = '/images/progress_bar_empty.png';
 
 
 const TimerComponent = ({time, pokemonUrl}:TimerComponentProp) => {
@@ -61,7 +63,9 @@ const TimerComponent = ({time, pokemonUrl}:TimerComponentProp) => {
         if (minutes == 0 && getSeconds() == 0) {
             // we count 1 completed pomodoro
             hatchCounter = hatchCounter + 1;
-            if (hatchCounter == 2){
+            incrementProgress(hatchCounter.toString());
+            //num.toString();
+            if (hatchCounter == 7){
                 hatchCounter = 0;
                 imgSource = pokemonUrl;
                 router.push('/hatch-egg');
@@ -82,6 +86,16 @@ const TimerComponent = ({time, pokemonUrl}:TimerComponentProp) => {
         return x
     }
 
+    const incrementProgress = (divID: string) => {
+        const currDiv = document.getElementById(divID) as HTMLImageElement;
+        if (currDiv != null){
+            currDiv.setAttribute("src", progressBarIconFull);
+        }
+        console.log("CHANGE")
+    }
+
+    //<Image id = "1" src={progressBarIconEmpty} alt = "icon" width={40} height={40}></Image>
+
     return (
         <div className="timer">
             <h2>{`${getMinutes()}:${getSeconds()}`}</h2>
@@ -90,6 +104,15 @@ const TimerComponent = ({time, pokemonUrl}:TimerComponentProp) => {
             <button className="pauseButton" onClick={resetTimer}> reset </button>
             <h1>{hatchCounter}</h1>
             <ImageLoaderComponent link= {imgSource}/>
+            <br></br>
+            <div className="progress bar" style = {{display: "flex"}}>
+                <img id = "1" src = {progressBarIconEmpty} width={"40px"}></img>
+                <img id = "2" src = {progressBarIconEmpty} width={"40px"}></img>
+                <img id = "3" src = {progressBarIconEmpty} width={"40px"}></img>
+                <img id = "4" src = {progressBarIconEmpty} width={"40px"}></img>
+                <img id = "5" src = {progressBarIconEmpty} width={"40px"}></img>
+                <img id = "6" src = {progressBarIconEmpty} width={"40px"}></img>
+            </div>
         </div>
     );
 };
